ade415121fcf59d80e49a3a0fa707072972dfbb5
John Doe <jdnieto78@gmail.com>
2025-03-12 11:11:25 +0800

    bug found, unresolved

--- guidance-counselor-student-management/app/routes.py
+++ guidance-counselor-student-management/app/routes.py
@@ -5,7 +5,7 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('view_records.html')
 
 @main.route('/student')
 def student():
