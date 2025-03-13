8ef2cd314ee9499c855dbb96f91b41de08a0fbf7
jlnmx <sabandojullian@gmail.com>
2025-03-13 11:24:02 +0800

    added add new student record feature

--- guidance-counselor-student-management/app/routes.py
+++ guidance-counselor-student-management/app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from .models import db, StudentRecord
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
@@ -30,22 +31,46 @@ def view_profile(student_id):
 def add_student():
     if request.method == 'POST':
         name = request.form['name']
-        reason = request.form['reason']
-        type_of_offense = request.form['type_of_offense']
-        additional_info = request.form['additional_info']
+        grade_section = request.form['grade_section']
+        birthdate = datetime.strptime(request.form['birthdate'], '%Y-%m-%d')
+        age = request.form['age']
+        mother_tongue = request.form['mother_tongue']
+        ethnic_group = request.form['ethnic_group']
+        religion = request.form['religion']
+        address_house_no = request.form['address_house_no']
+        address_barangay = request.form['address_barangay']
+        address_city = request.form['address_city']
+        address_province = request.form['address_province']
+        mother_name = request.form['mother_name']
+        father_name = request.form['father_name']
+        guardian_name = request.form['guardian_name']
+        contact_number = request.form['contact_number']
+        learning_modality = request.form['learning_modality']
 
         new_record = StudentRecord(
             name=name,
-            reason=reason,
-            type_of_offense=type_of_offense,
-            additional_info=additional_info
+            grade_section=grade_section,
+            birthdate=birthdate,
+            age=age,
+            mother_tongue=mother_tongue,
+            ethnic_group=ethnic_group,
+            religion=religion,
+            address_house_no=address_house_no,
+            address_barangay=address_barangay,
+            address_city=address_city,
+            address_province=address_province,
+            mother_name=mother_name,
+            father_name=father_name,
+            guardian_name=guardian_name,
+            contact_number=contact_number,
+            learning_modality=learning_modality
         )
         db.session.add(new_record)
         db.session.commit()
 
         return redirect(url_for('main.manage_records'))
 
-    return render_template('add_student_offense.html')
+    return render_template('add_new_student.html')
 
 @main.route('/add_report', methods=['GET', 'POST'])
 def add_report():
