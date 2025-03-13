d5705c25e19837fe2ec534f1df6c2f958566945e
jlnmx <sabandojullian@gmail.com>
2025-03-13 13:38:47 +0800

    edited the title, updated the database by adding user table, login and registration form are already functional

--- guidance-counselor-student-management/app/models.py
+++ guidance-counselor-student-management/app/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class StudentRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,3 +24,15 @@ class StudentRecord(db.Model):
     type_of_offense = db.Column(db.String(100), nullable=True)
     date_time = db.Column(db.DateTime, default=datetime.utcnow)
     additional_info = db.Column(db.Text, nullable=True)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
