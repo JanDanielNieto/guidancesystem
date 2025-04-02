6f69b5999f3cc61e7222007093c96a16aaf3182a
John Doe <jdnieto78@gmail.com>
2025-04-02 16:00:09 +0800

    Added more buttons

--- guidance-counselor-student-management/frontend/src/pages/Dashboard.js
+++ guidance-counselor-student-management/frontend/src/pages/Dashboard.js
@@ -3,10 +3,16 @@ import '../css/styles.css';
 
 const Dashboard = () => {
   return (
-    <div>
+    <div class="dashboard-container">
       <h1>Welcome to the Guidance System</h1>
       <p>Manage student records and offenses efficiently.</p>
+      <div class="button-container">
       <button onClick={() => window.location.href = '/students'}>View Students</button>
+      <button onClick={() => window.location.href = '/students'}>Manage Records</button>
+      <button onClick={() => window.location.href = '/students'}>Add Report</button>
+      <button onClick={() => window.location.href = '/students'}>Manage Students</button>
+      <button onClick={() => window.location.href = '/students'}>Analytics</button>
+      </div>
     </div>
   );
 };
