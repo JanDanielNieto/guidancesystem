a113251f8c483444684b21d88179b2dff0a60e9f
jlnmx <sabandojullian@gmail.com>
2025-04-03 12:42:32 +0800

    changes on dashboard

--- guidance-counselor-student-management/frontend/src/pages/Dashboard.js
+++ guidance-counselor-student-management/frontend/src/pages/Dashboard.js
@@ -7,10 +7,9 @@ const Dashboard = () => {
       <h1>Welcome to the Guidance System</h1>
       <p>Manage student records and offenses efficiently.</p>
       <div class="button-container">
-      <button onClick={() => window.location.href = '/students'}>View Students</button>
+      <button onClick={() => window.location.href = '/students'}>Manage Students</button>
       <button onClick={() => window.location.href = '/records'}>Manage Records</button>
       <button onClick={() => window.location.href = '/addreport'}>Add Report</button>
-      <button onClick={() => window.location.href = '/editstudent'}>Manage Students</button>
       <button onClick={() => window.location.href = '/analytics'}>Analytics</button>
       </div>
     </div>
