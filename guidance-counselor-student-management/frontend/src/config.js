b56e04b35a086eeb6574f38535d47d2dd7bc99e7
jlnmx <sabandojullian@gmail.com>
2025-05-07 14:34:57 +0800

    fix: Update REACT_APP_API_URL and API_BASE_URL for consistency

--- guidance-counselor-student-management/frontend/src/config.js
+++ guidance-counselor-student-management/frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: 'https://guidancesystem-9vgv.onrender.com', 
-  };
-  
-  export default config;
+  API_BASE_URL: 'https://guidancesystem-9vgv.onrender.com', 
+};
+
+export default config;
